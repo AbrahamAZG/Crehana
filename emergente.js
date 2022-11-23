07eed59e831fc5be4b7c4ae42a170d7bd2ed08d7
Abrahamazg <abrahamazg@hotmail.com>
2022-11-23 12:03:26 -0500

    avance

--- emergente.js
+++ emergente.js
@@ -2,10 +2,17 @@
         const emergente = document.querySelector('.imagencursos')
         const cajaemergente = document.querySelector('.cajaemergente')
         const cerrito = document.querySelector('.cerrito')
+        const todo = document.querySelector('body')
         emergente.addEventListener('click',function(){
                 cajaemergente.classList.toggle('active')
+                todo.classList.add('color')
+                cajaemergente.classList.add('quitar')
+                emergente.classList.add('colores')
         })
         cerrito.addEventListener('click',function(){
                 cajaemergente.classList.toggle('active')
+                todo.classList.remove('color')
+                cajaemergente.classList.remove('quitar')
+                emergente.classList.remove('colores')
         })
         
