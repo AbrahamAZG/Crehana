1330471e281b578db2aa09ca811c39f958d10d87
Abrahamazg <abrahamazg@hotmail.com>
2022-11-25 11:46:01 -0500

    avance

--- responsive.js
+++ responsive.js
@@ -2,6 +2,8 @@ const resp = document.querySelector('.menuresp')
 const lineas = document.querySelector('.menuresponsive')
 let cuerpo = document.querySelector('body')
 const cerrarr = document.querySelector('.cerrar')
+const dropdownmenus = document.querySelector('.dropdownmenu')
+const dropdowncursoss = document.querySelector('.dropdownmenucursosh')
 
 lineas.addEventListener('click',function(){
    resp.classList.toggle('menurespoToggle');
@@ -10,4 +12,9 @@ lineas.addEventListener('click',function(){
 })
 cerrarr.addEventListener('click',function(){
     resp.classList.toggle('menurespoToggle')
+})
+
+dropdowncursoss.addEventListener('click',function(){
+    dropdownmenus.classList.toggle('activatedrop')
+    dropdownmenus.style.marginTop = '10px'
 })
